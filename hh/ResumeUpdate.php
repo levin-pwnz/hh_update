e8791fa18d837e7c6bf85c203e4d71c3954a2171
Dmitry <iglass73@gmail.com>
2017-08-16 16:48:50 +0800

    First commit

---
+++ hh/ResumeUpdate.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry^
+ */
+
+namespace HHUpdate;
+
+class ResumeUpdate
+{
+    private $resume;
+    private $token;
+    private $smstoken;
+    private $phone;
+
+
+    function __construct($resume, $token, $smstoken, $phone)
+    {
+        $this->resume = $resume;
+        $this->token = $token;
+        $this->smstoken = $smstoken;
+        $this->phone = $phone;
+
+        if (!$resume || !$token || !$smstoken || !$phone) {
+
+            exit("Некоторые параметры конфига путые. Пожалуйста проверьте index.php");
+        }
+    }
+
+    private function SendSms($msg)
+    {
+        $ch = curl_init("https://sms.ru/sms/send");
+        $data =
+            [
+                "to" => $this->phone,
+                "msg" => $msg,
+                "api_id" => $this->smstoken
+            ];
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+        return curl_exec($ch);
+
+
+    }
+
+    public function UpdateResume()
+    {
+        $ch = curl_init("https://api.hh.ru/resumes/" . $this->resume . "/publish");
+
+        $data =
+            [
+                "resume_id" => $this->resume
+            ];
+
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer " . $this->token));
+
+        $result = curl_exec($ch);
+        $result = json_decode($result, true);
+
+        if (empty($result)) {
+            $status = "Resume update complete!";
+        } else {
+            $status = $result['description'];
+        }
+
+        curl_close($ch);
+
+        return $this->SendSms($status);
+    }
+}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitry^
+ */
+
+require_once "hh/ResumeUpdate.php";
+
+use HHUpdate\ResumeUpdate as ResumeUpdate;
+
+/* Config */
+$HHResume = ""; // ID Резюме
+$HHToken = ""; // Токен для доступа на HeadHunter
+$SMSToken = ""; //SMS.ru Токен для отправки смс
+$Phone = ""; //Ваш номер телефона
+$Phone = ""; //Ваш номер телефона
+
+/* Обновление резюме */
+$Update = new ResumeUpdate($HHResume, $HHToken, $SMSToken, $Phone); //Входные параметры
+$Update->UpdateResume(); // Вызов функции обновления
